bfcea7f48d227248eb106af57e50fdf05a5b7a6e
Affapple <joaopmgaspar@ua.pt>
2025-10-09 15:05:20 +0200

    Added some basis to application entrypoint

--- escape.py
+++ escape.py
@@ -1,1 +1,4 @@
 # This file is to parse input to initiate game
+# library argparser
+inputs = ["--start", "intro", "--transcript", "run.txt"] {"start": "intro", "transcript": "run.txt"}-> 
+inputs -> escaperoom/engine.py
